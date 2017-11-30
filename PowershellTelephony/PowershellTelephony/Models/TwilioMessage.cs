b09f26dcbc895cf7ac6467b429478144603cae1a
Kirkland <kirkland@yuknis.com>
2017-11-29 21:34:06 -0500

    Initial Commit
    Added some basic scaffolding, required libraries, and a container object for an SMS message.

---
+++ PowershellTelephony/PowershellTelephony/Models/TwilioMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerShellModules.Models
+{
+    public class TwilioMessage
+    {
+        public string AccountSid { get; set; }
+        public string AuthToken { get; set; }
+        public string Recepient { get; set; }
+        public string Sender { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ PowershellTelephony/PowershellTelephony/NewSmsMessageCmdlet.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Management.Automation;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+using PowerShellModules.Models;
+
+namespace PowerShellModules
+{
+    [Cmdlet(VerbsCommon.New, "SmsMessage")]
+    [OutputType(typeof(TwilioMessage))]
+    public class NewSmsMessageCmdlet: Cmdlet
+    {
+        [Parameter]
+        public string AccountSid { get; set; }
+
+        [Parameter(ValueFromPipeline = true)]
+        public string AuthToken { get; set; }
+
+        [Parameter]
+        public string Recipient { get; set; }
+
+        [Parameter]
+        public string Sender { get; set; }
+
+        [Parameter(ValueFromPipeline = true)]
+        public string Body { get; set; }
+
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+        }
+
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+        }
+
+        protected override void StopProcessing()
+        {
+            base.StopProcessing();
+        }
+    }
+}
